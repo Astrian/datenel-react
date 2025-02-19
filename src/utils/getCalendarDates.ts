6b2af762f9dce335db64751373b5455848e234be
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-02-19 16:41:03 +1100

    refactor: adjust calendar start date calculation to handle week alignment correctly

--- src/utils/getCalendarDates.ts
+++ src/utils/getCalendarDates.ts
@@ -2,8 +2,11 @@ export default (currentDate: number, currentYear: number): Date[] => {
 	const baselineDate = new Date(currentYear, currentDate)
 
 	const calendarStart = new Date(baselineDate.getFullYear(), baselineDate.getMonth(), 1)
-	if (calendarStart.getDay() !== 1) 
-		calendarStart.setDate(calendarStart.getDate() - calendarStart.getDay() + 1)
+
+	const dayOfWeek = calendarStart.getDay() === 0 ? 7 : calendarStart.getDay()
+	
+	if (dayOfWeek !== 1) 
+		calendarStart.setDate(calendarStart.getDate() - dayOfWeek + 1)
 	
 	const calendarEnd = new Date(baselineDate.getFullYear(), baselineDate.getMonth() + 1, 0)
 	if (calendarEnd.getDay() !== 0)
