48d3b6f5597123d23e41091f72d5955fa275148b
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-02-19 15:26:11 +1100

    refactor: update weekday localization to use narrow format in getL10Weekday utility

--- src/utils/getL10Weekday.ts
+++ src/utils/getL10Weekday.ts
@@ -2,7 +2,7 @@ export default (userLang: string) => {
 	const newL10nDays = []
 	for (let i = 0; i < 7; i++) {
 		const date = new Date(2021, 0, i + 4)
-		newL10nDays.push(date.toLocaleDateString(userLang, {weekday: 'short'}))
+		newL10nDays.push(date.toLocaleDateString(userLang, {weekday: "narrow"}))
 	}
 	return newL10nDays
 }
