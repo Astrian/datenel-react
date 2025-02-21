4b0b821da58cd4ed146aabe0538c22df0a6edcd8
Astrian Zheng <happy.wheel6743@astrian.moe>
2025-02-21 17:42:31 +1100

    feat: add available date range feature to SingleDatePicker component

--- playground/app.tsx
+++ playground/app.tsx
@@ -6,7 +6,15 @@ export default () => {
 
 	return (<div className='app'>
 		<div className="border">
-			<SingleWeekPicker value={new Date(2025, 0, 1)} onSelect={(date) => console.log(date)} />
+			<SingleDatePicker availableRange={[{
+				year: 2023,
+				month: 1,
+				day: 1
+			}, {
+				year: 2023,
+				month: 12,
+				day: 31
+			}]} />
 		</div>
 	</div>)
 }
